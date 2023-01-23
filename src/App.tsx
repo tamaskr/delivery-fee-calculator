a99d9c14517d602e502a350750f8e45f61a1b5ca
Tamas Kralusz <tamas.kralusz@reactron.dev>
2023-01-23 17:56:18 +0200

    update eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import './App.css';
 
 const App = () => {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<p>Delivery fee calculator</p>
-			</header>
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Delivery fee calculator</p>
+      </header>
+    </div>
+  );
 };
 
 export default App;
