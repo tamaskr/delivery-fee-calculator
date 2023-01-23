d1115177196de4178a026a217cb9202ee5afada5
Tamas Kralusz <tamas.kralusz@reactron.dev>
2023-01-23 18:25:18 +0200

    add i18n localization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { useTranslation } from 'react-i18next';
 import './App.css';
 
 const App = () => {
+  const { t } = useTranslation();
   return (
     <div className="App">
       <header className="App-header">
-        <p>Delivery fee calculator</p>
+        <p>{t('general.title')}</p>
       </header>
     </div>
   );
